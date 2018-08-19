96cdf66fc412310c523740eeb5b160bda89ac0b3
charliesands <cwsands3@gmail.com>
2018-08-19 15:32:22 -0600

    more styling

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,7 +1,5 @@
 import TodoService from "./todo-service.js";
 
-
-
 var todoService = new TodoService
 
 // Use this getTodos function as your callback for all other edits
@@ -20,18 +18,16 @@ function draw(todos) {
     if (todo.completed) {
 
       template += `
-        <div>
-      <input type="checkbox" id="todo-check" name="todo-list-check" value="todo" onchange="app.controllers.todoController.toggleTodoStatus('${todo._id}')" checked />
-      ${todo.description} <i class="fas fa-trash" onclick="app.controllers.todoController.removeTodo('${todo._id}')"type="submit"></i> 
-      
+      <div>
+        <input type="checkbox" id="todo-check" name="todo-list-check" value="todo" onchange="app.controllers.todoController.toggleTodoStatus('${todo._id}')" checked />
+        ${todo.description} <i class="fas fa-trash" onclick="app.controllers.todoController.removeTodo('${todo._id}')"type="submit" title="Delete"></i> 
       </div>
       `
     } else {
       template += `
       <div>
-      <input type="checkbox" id="todo-check" name="todo-list-check" value="todo" onchange="app.controllers.todoController.toggleTodoStatus('${todo._id}')" />
-      ${todo.description} <i class="fas fa-trash" onclick="app.controllers.todoController.removeTodo('${todo._id}')"type="submit"></i> 
-      
+        <input type="checkbox" id="todo-check" name="todo-list-check" value="todo" onchange="app.controllers.todoController.toggleTodoStatus('${todo._id}')" />
+        ${todo.description} <i class="fas fa-trash" onclick="app.controllers.todoController.removeTodo('${todo._id}')"type="submit" title="Delete"></i> 
       </div>
             `
     }
@@ -47,8 +43,6 @@ function draw(todos) {
 }
 //DONT FORGET TO LOOP
 
-
-
 export default class TodoController {
   constructor() {
     getTodos()
@@ -62,7 +56,6 @@ export default class TodoController {
   // removeTodo takes in a todoId and sends a delete request to the server
   // **** HINT: Everytime you make a change to any todo don't forget to get the todo list again
 
-
   addTodoFromForm(e) {
 
     e.preventDefault() // <-- hey this time its a freebie don't forget this
@@ -70,11 +63,9 @@ export default class TodoController {
     var form = e.target
     var todo = {
       description: form.todo.value
-
       // DONT FORGET TO BUILD YOUR TODO OBJECT
     }
     console.log(todo)
-
     //PASSES THE NEW TODO TO YOUR SERVICE
     //DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
     //YOU SHOULDN'T NEED TO CHANGE THIS
@@ -93,84 +84,7 @@ export default class TodoController {
     todoService.removeTodo(todoId, draw)
     // ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
   }
-
-
-
 }
 
 
-// import TodoService from "./todo-service.js";
 
-
-
-// var todoService = new TodoService
-
-
-// // Use this getTodos function as your callback for all other edits
-// function getTodos() {
-//   //FYI DONT EDIT ME :)
-//   todoService.getTodos(draw)
-// }
-
-// function draw(todos)
-// //WHAT IS MY PURPOSE?
-// // let todos = todoService.getTodos()
-// //BUILD YOUR TODO TEMPLATE HERE
-// // let template = ""
-// // for (let i = 0; i < getTodos.length; i++) {
-// // 	const todo = getTodos[i];
-
-
-// // 	template += `
-// // 		<div>${todo.description}</div>
-// // 		`
-
-// // }
-
-// //DONT FORGET TO LOOP
-
-
-
-// export default class TodoController {
-//   constructor() {
-
-//     // IF YOU WANT YOUR TODO LIST TO DRAW WHEN THE PAGE FIRST LOADS WHAT SHOULD YOU CALL HERE???
-//   }
-//   // You will need four methods
-//   // getTodos should request your api/todos and give an array of todos to your callback fn
-//   // addTodo takes in a todo and posts it to the server
-//   // toggleTodoStatus takes in a todo marks its status as completed and puts it to the server
-//   // removeTodo takes in a todoId and sends a delete request to the server
-//   // **** HINT: Everytime you make a change to any todo don't forget to get the todo list again
-
-
-//   addTodoFromForm(e) {
-//     e.preventDefault() // <-- hey this time its a freebie don't forget this
-//     // TAKE THE INFORMATION FORM THE FORM
-//     var form = e.target
-//     var todo = {
-//       // DONT FORGET TO BUILD YOUR TODO OBJECT
-//     }
-
-//     //PASSES THE NEW TODO TO YOUR SERVICE
-//     //DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
-//     //YOU SHOULDN'T NEED TO CHANGE THIS
-//     todoService.addTodo(todo, getTodos)
-//     //^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
-//   }
-
-//   toggleTodoStatus(todoId) {
-//     // asks the service to edit the todo status
-//     // todoService.toggleTodoStatus(todoId, getTodos)
-//     // YEP THATS IT FOR ME
-//   }
-
-//   removeTodo(todoId) {
-//     // ask the service to run the remove todo with this id
-
-//     // ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
-//   }
-
-
-
-// }
